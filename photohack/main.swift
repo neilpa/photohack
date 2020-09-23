c988c576b1dc92ef6f457f72daf29864e532a6bd
Neil Pankey <npankey@gmail.com>
2020-09-23 16:24:53 -0700

    photohack: embed and extract Info.plist
    Works around the OS not checking the embedded plist so that photohack
    can request permissions for photo library access without crashing.

--- photohack/main.swift
+++ photohack/main.swift
@@ -6,6 +6,24 @@ if CommandLine.arguments.count < 3 {
     exit(1)
 }
 
+// Embedding the Info.plist isn't sufficient to access the photos library and will still crash with the
+// following message:
+//
+//  photohack[18242:3469015] [access] This app has crashed because it attempted to access privacy-sensitive
+//  data without a usage description.  The app's Info.plist must contain an NSPhotoLibraryUsageDescription
+//  key with a string value explaining to the user how the app uses this data.
+//
+// Extracting and writing alongside the executable seems to work and avoids having to copy an extra file.
+// TODO: This may cause issues if not placed in a user-writeable location.
+do {
+    let b = Bundle.main
+    let plist = CFBundleCopyInfoDictionaryForURL(b.executableURL! as CFURL) as NSDictionary
+    let url = URL.init(fileURLWithPath: "Info.plist", relativeTo: b.resourceURL)
+    try plist.write(to: url)
+} catch {
+    print(error)
+}
+
 let sem = DispatchSemaphore(value: 0);
 PHPhotoLibrary.requestAuthorization({(status:PHAuthorizationStatus) in
     switch status {
@@ -18,7 +36,7 @@ PHPhotoLibrary.requestAuthorization({(status:PHAuthorizationStatus) in
 })
 sem.wait();
 
-print(PhotoProxy.entityToClass() as AnyObject)
+//print(PhotoProxy.entityToClass() as AnyObject)
 
 let proxy = PhotoProxy.fromPath(CommandLine.arguments[1])!
 let uuids = CommandLine.arguments.dropFirst(2).map { UUID(uuidString: $0)! };
