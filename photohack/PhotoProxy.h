352f0d5f6075bbcc70df01a6a16ecbc90e9b371b
Neil Pankey <npankey@gmail.com>
2020-10-07 23:18:27 -0700

    jpegs: Command for exporting adjusted JPEGs

--- photohack/PhotoProxy.h
+++ photohack/PhotoProxy.h
@@ -15,7 +15,15 @@
 
 // Open the *.photoslibrary package at the given path
 + (PhotoProxy*) fromPath:(NSString*)path;
-// Returns a mapping of provided UUID values to adjustment dictionaries.
+// Fetches assets matching the provided UUIDs
+- (NSArray*) fetchAssets:(NSArray*)uuids;
+
+// Returns a mapping of provided UUID values to adjustment dictionaries
 - (NSDictionary*) fetchAdjustments:(NSArray*)uuids;
+// Looks up adjustments for the provided asset
+- (NSDictionary*) adjustments:(PHAsset*)asset;
+
+// Write potentially adjusted JPEGs in a directory, each named by UUID
+- (void) exportJPEGs:(NSArray*)uuids toDir:(NSString*)path;
 
 @end
